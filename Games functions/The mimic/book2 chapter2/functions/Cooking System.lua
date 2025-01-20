35e12931fbb416c3446322711ca816c7a4d53b59
Rael Hub <israelbiscoitinho@gmail.com>
2025-01-20 11:58:44 -0300

    update

--- Games functions/The mimic/book2 chapter2/functions/Cooking System.lua
+++ Games functions/The mimic/book2 chapter2/functions/Cooking System.lua
@@ -1,7 +1,7 @@
 local CookingSystem = {}
 
-getgenv().ChooseStove = "1"
-getgenv().ChooseStove = "1"
+getgenv().ChooseStove = 1
+getgenv().ChooseBench = 1
 
 function GetFolderCooking()
   
@@ -61,24 +61,113 @@ function CookingSystem.GetBenches()
     
   }
   
+  return BenchTable
+  
 end
 
-function CookingSystem.MakeFood(Position)
+function CookingSystem.MakeFood(food)
   
-  local function Teleport()
+  if not shared.FolderCooking then
+    
+    warn("[Error] FolderCooking does not exist")
+    return nil
+    
+  end
+  
+  local function TeleportActivePrompt(Position, Prompt)
     
     local HumanoidRootPart = shared.Character:FindFirstChild("HumanoidRootPart")
     
     if HumanoidRootPart then
       
       HumanoidRootPart.CFrame = Position
+      task.wait(1)
+      fireproximityprompt(Prompt)
+      
+    end
+  end
+  
+  local function FoodGetorSet(foodname)
+    
+    local Givers =  shared.FolderCooking:FindFirstChild("Givers")
+    
+    if Givers then
+      
+      local Handle = Givers[foodname]:FindFirstChild("Handle")
+      
+      local Prompt = Handle:FindFirstChildOfClass("ProximityPrompt")
+      
+      TeleportActivePrompt(CFrame(Handle.Position), Prompt)
+    end
+  end
+  
+  local function InteractWithTheBench()
+    
+    local TableBenches = CookingSystem.GetBenches()
+    
+    if not TableBenches then
+      
+      warn("[Erro] TableBenches is nil")
+      return nil
       
     end
     
+    local function GetBaseAndPrompt(model)
+      
+      for _, Obj in ipairs(model:GetChildren()) do
+        
+        if Obj:IsA("BasePart") then
+          
+          local Prompt = Obj:FindFirstChildOfClass("ProximityPrompt")
+          
+          if Prompt then
+            
+            return Obj, Prompt
+            
+          end
+        end
+      end
+    end
+    
+    local BasePart, Prompt = GetBaseAndPrompt(TableBenches[getgenv().ChooseBench])
+    
+    TeleportActivePrompt(CFrame(BasePart.Position), Prompt)
+    
   end
   
+  local function InteractWithTheSolve()
+    
+    local TabletStove = CookingSystem.StoveTable()
+    
+    if not TabletStove then
+      
+      warn("[Erro] TabletSolve is nil")
+      return nil
+      
+    end
+    
+    local Detector = TabletStove[getgenv().ChooseStove]:FindFirstChild("Detector")
+    
+    local Prompt = Detector:FindFirstChild("ProximityPrompt")
+    
+    TeleportActivePrompt(CFrame(Detector.Position), Prompt)
+    
+  end
   
-  
+  if food == "Chicken" then
+    
+    
+    FoodGetorSet("Pot")
+    InteractWithTheBench()
+    FoodGetorSet("Chicken")
+    InteractWithTheBench()
+    FoodGetorSet("Wrapped Meat")
+    InteractWithTheBench()
+    FoodGetorSet("Cheese")
+    InteractWithTheBench()
+    InteractWithTheBench()
+    
+  end
 end
 
 GetFolderCooking()
