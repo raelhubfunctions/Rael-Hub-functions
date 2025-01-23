9aa2c6d0adbd35b735c655cd2f3eb25d3181f1f1
Rael Hub <israelbiscoitinho@gmail.com>
2025-01-23 17:29:05 -0300

    update

---
+++ Games functions/The mimic/book2 chapter2/functions/Boss System.lua
@@ -0,0 +1,76 @@
+local NagisaSystem = {}
+
+function NagisaSystem.FindFolder()
+  for _, pasta in ipairs(Workspace:GetChildren()) do
+    if pasta:IsA("Folder") then
+      for _, model in ipairs(pasta:GetChildren()) do
+        if model:IsA("Folder") and model.Name == "Stage2BOSS" then
+          
+          warn("[Rael Hub] FolderNagisaBoss was found")
+          shared.FolderNagisaBoss = pasta
+          return pasta
+          
+        end
+      end
+    end
+  end
+  return nil
+end
+
+function NagisaSystem.AntiLeizer(value)
+  
+  if not shared.FolderNagisaBoss then
+    
+    warn("[Erro] FolderNagisaBoss not found")
+    
+  end
+
+  getgenv().AntiLeizer = value
+  
+  while getgenv().AntiLeizer do
+    
+    local LeizerPart = shared.FolderNagisaBoss:FindFirstChild("POISON", true)
+    
+    if LeizerPart.Transparency == 0.5 then
+      shared.Character.HumanoidRootPart.CFrame = CFrame.new(1779.863037109375, 233.0954132080078, -4773.52490234375)
+    end
+    
+    task.wait()
+    
+  end
+end
+
+function NagisaSystem.AutoCollectBala(value)
+  
+  if not shared.FolderNagisaBoss then
+    
+    warn("[Erro] FolderNagisaBoss not found")
+    
+  end
+  
+  
+  getgenv().AutoCollectBala = value
+  
+  while getgenv().AutoCollectBala do
+  
+    local InteractPart = shared.FolderNagisaBoss:FindFirstChild("Interact", true)
+    
+    if InteractPart and InteractPart:IsA("BasePart") and tostring(InteractPart.Parent) == "BallGiver" then
+      
+      local Prompt = InteractPart:FindFirstChildOfClass("ProximityPrompt")
+      
+      if Prompt then
+        fireproximityprompt(Prompt)
+        task.wait(0.5)
+      end
+    end
+    task.wait()
+    
+  end
+end
+
+NagisaSystem.FindFolder()
+NagisaSystem.AntiLeizer(false)
+NagisaSystem.AutoCollectBala(false)
+
+return NagisaSystem
