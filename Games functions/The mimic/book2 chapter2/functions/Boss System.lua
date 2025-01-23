2a7d35be1bf59856273fd8486aa4398ed516f07d
Rael Hub <israelbiscoitinho@gmail.com>
2025-01-23 18:07:52 -0300

    update Boss System.lua

--- Games functions/The mimic/book2 chapter2/functions/Boss System.lua
+++ Games functions/The mimic/book2 chapter2/functions/Boss System.lua
@@ -1,4 +1,22 @@
 local NagisaSystem = {}
+local VirtualUser = game:GetService("VirtualUser")
+
+function NagisaSystem.AutoClicker(value)
+  
+  function autoClick()
+    VirtualUser:CaptureController()
+    VirtualUser:Button1Down(Vector2.new(1e4, 1e4))
+    task.wait(0.05)
+  end
+  
+  getgenv().RaelHubAutoClicker = value
+  
+  task.spawn(function()
+    while getgenv().RaelHubAutoClicker do
+      autoClick()
+    end
+  end)
+end
 
 function NagisaSystem.FindFolder()
   for _, pasta in ipairs(Workspace:GetChildren()) do
