641f699053b2973444b34a9f55c41ee73c417f3c
Rael Hub <israelbiscoitinho@gmail.com>
2025-01-26 02:02:10 -0300

    update ModuleSystem.lua

--- Games functions/The mimic/book1 chapter4/Module/ModuleSystem.lua
+++ Games functions/The mimic/book1 chapter4/Module/ModuleSystem.lua
@@ -3,26 +3,61 @@ local Players = game.Players
 local LocalPlayer = Players.LocalPlayer
 local Backpack = LocalPlayer.Backpack
 
+local VirtualUser = game:GetService("VirtualUser")
+
 local RaelHubChapter4Module = {}
 
-function CheckPart4()
-  
-  local Ids = {7251867574, 7265397848}
-  
+function RaelHubChapter4Module.CheckPart1()
+  local Ids = {7251865082, 7265396387}
   if table.find(Ids, MapID) then
-    
     return true
-    
   end
-
 end
+function RaelHubChapter4Module.CheckPart2()
+  local Ids = {7251866503, 7265396805}
+  if table.find(Ids, MapID) then
+    return true
+  end
+end
+function RaelHubChapter4Module.CheckPart3()
+  local Ids = {7251867155, 7265397072}
+  if table.find(Ids, MapID) then
+    return true
+  end
+end
+function RaelHubChapter4Module.CheckPart4()
+  local Ids = {7251867574, 7265397848}
+  if table.find(Ids, MapID) then
+    return true
+  end
+end
+
+
+
+function RaelHubChapter4Module.AutoClicker(value)
   
+  function autoClick()
+    VirtualUser:CaptureController()
+    VirtualUser:Button1Down(Vector2.new(1e4, 1e4))
+    task.wait(0.05)
+  end
+  
+  getgenv().RaelHubAutoClicker = value
+  
+  task.spawn(function()
+    while getgenv().RaelHubAutoClicker do
+      autoClick()
+    end
+  end)
+end
+
+
 
 function RaelHubChapter4Module.EquipKatana(value)
   
   getgenv().EquipKatana = value
   
-  if not CheckPart4() then
+  if not RaelHubChapter4Module.CheckPart4() then
     
     return nil
     
@@ -54,6 +89,9 @@ function RaelHubChapter4Module.EquipKatana(value)
   end)
 end
 
+
+
+RaelHubChapter4Module.AutoClicker(false)
 RaelHubChapter4Module.EquipKatana(false)
 
 return RaelHubChapter4Module
