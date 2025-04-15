873c96913dbdb6e19cd5390c390b6f050df0394f
Rael Hub <israelbiscoitinho@gmail.com>
2025-04-15 19:25:16 -0300

    update testTraducao.lua

--- Games functions/Funky friday/testTraducao.lua
+++ Games functions/Funky friday/testTraducao.lua
@@ -3,54 +3,54 @@ local HttpService = game:GetService("HttpService")
 local LocalizationService = game:GetService("LocalizationService")
 
 function Module.GetPlayerLanguage()
-  local result, code = pcall(function()
-    return LocalizationService.RobloxLocaleId
-  end)
-  if result then
-    return code:sub(1, 2)
-  else
-    return "en"
-  end
+	local result, code = pcall(function()
+		return LocalizationService.RobloxLocaleId
+	end)
+	if result then
+		return code:sub(1, 2) -- Pega só os dois primeiros caracteres, tipo "en"
+	else
+		return "en" -- Padrão caso dê erro
+	end
 end
 
 local language = Module.GetPlayerLanguage()
 
 function Module.SaveTranslation(nameFolder, version, TableTranslation)
-  if not isfolder(nameFolder) then
-    makerfolder(nameFolder)
-  end
-  local fileName = nameFolder .. "/" .. language .. " " .. version .. ".json"
-  local json = HttpService:JSONEncode(TableTranslation)
-  writefile(fileName, json)
+	if not isfolder(nameFolder) then
+		makefolder(nameFolder)
+	end
+	local fileName = nameFolder .. "/" .. language .. " " .. version .. ".json"
+	local json = HttpService:JSONEncode(TableTranslation)
+	writefile(fileName, json)
 end
 
 function Module.LoadTranslation(nameFolder, version)
-  local fileName = nameFolder .. "/" .. language .. " " .. version .. ".json"
-  if isfile(fileName) then
-    local json = readfile(fileName)
-    return HttpService:JSONDecode(json)
-  else
-    warn("[Erro] File does not exist")
-    return nil
-  end
+	local fileName = nameFolder .. "/" .. language .. " " .. version .. ".json"
+	if isfile(fileName) then
+		local json = readfile(fileName)
+		return HttpService:JSONDecode(json)
+	else
+		warn("[Erro] O arquivo não existe")
+		return nil
+	end
 end
 
 function Module.startVerification()
-  if getgenv().RaelHubAutoTranslator == nil then
-    getgenv().RaelHubAutoTranslator = true
-    warn("RaelHubAutoTranslator was created and set to true.")
-  else
-    warn("RaelHubAutoTranslator already exists, keeping the current value.")
-  end
+	if getgenv().RaelHubAutoTranslator == nil then
+		getgenv().RaelHubAutoTranslator = true
+		warn("RaelHubAutoTranslator foi criado e definido como true.")
+	else
+		warn("RaelHubAutoTranslator já existe, mantendo o valor atual.")
+	end
 end
 
-function Module.ReturnLoadedTranslations()
-  local TableValues = {}
-  for _, Table in pairs(versionTranslation) do
-    table.insert(TableValues, Table)
-    print(Table)
-  end
-  return table.unpack(TableValues)
+function Module.ReturnLoadedTranslations(TableTranslation)
+	local TableValues = {}
+	for _, Table in pairs(TableTranslation) do
+		table.insert(TableValues, Table)
+		print(Table)
+	end
+	return table.unpack(TableValues)
 end
 
 return Module
