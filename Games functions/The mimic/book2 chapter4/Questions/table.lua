b5f381bfdb421623ffc2eb00b6302396884873b1
raelhubfunctions <raeldevminecraft@gmail.com>
2025-06-05 00:08:44 -0300

    Create table.lua

---
+++ Games functions/The mimic/book2 chapter4/Questions/table.lua
@@ -0,0 +1,41 @@
+local answerCheatSheet = {
+    {
+        Question = "I am pathetic, regretful, and I'm the reason my brother died. Who am I?",
+        response = "Me"
+    },
+    {
+        Question = "Flames consume at my decree",
+        response = "Enzukai"
+    },
+    {
+        Question = "I sit quietly, made of stone, and show the way to a sacred throne. What am I?",
+        response = "Torii Gate"
+    },
+    {
+        Question = "I have no voice, yet I tell stories in ink. I'm soft in your hand, but I carve into time. What am I?",
+        response = "A brush"
+    },
+    {
+        Question = "What is our cult name?",
+        response = "Kiiroibara Cult"
+    },
+    {
+        Question = "I bloom in spring, soft and sweet, pink and white, on branches I greet. I am Kintoru's favorite. What am I?",
+        response = "Cherry Blossom"
+    },
+    {
+        
+        Question = "I guard the shrine in silence",
+        response = "A komainu"
+    },
+    {
+        Question = "Four I shaped",
+        response = "Evil God"
+    },
+    {
+        Question = "I was once a curious girl",
+        response = " Hiachi Masashige"
+    }
+}
+
+return answerCheatSheet
