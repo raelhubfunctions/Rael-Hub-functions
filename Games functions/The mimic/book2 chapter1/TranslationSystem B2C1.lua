faee743f27c816fd2c6e57b7fcd8c505191b1d55
raelhubfunctions <raeldevminecraft@gmail.com>
2025-06-27 14:27:46 -0300

    Update TranslationSystem B2C1.lua

--- Games functions/The mimic/book2 chapter1/TranslationSystem B2C1.lua
+++ Games functions/The mimic/book2 chapter1/TranslationSystem B2C1.lua
@@ -2,38 +2,38 @@ local TranslationModule = {}
 
 function TranslationModule:GetTabs()
 
-    Tab_Oficine = {
-        name = "Teleporte sala",
-        section = "Teleportar para a sala",
-        button = "Teleportar pra sala"
+    local Tab_Oficine = {
+        name = "Teleport Room",
+        section = "Teleport to the room",
+        button = "Teleport to room"
     }
 
-    Tab_Sisters = {
-        name = "Fase das Irmãs",
-        section1 = "Ler livro na sala",
-        section2 = "Esp monstro",
-        section3 = "Auto coletar os ratos",
-        section4 = "Auto escapar dessa sala",
-        section5 = "Auto escapar de Rin",
-        button1 = "Ler livro",
-        button2 = "Esp rin",
-        button3 = "Auto coletar",
-        button4 = "Auto escapar",
-        button5 = "Auto fugir de Rin",
-        Notification = "Carregando...",
-        NotificationContent = "Processando arquivos"
+    local Tab_Sisters = {
+        name = "Sisters Phase",
+        section1 = "Read book in the room",
+        section2 = "ESP monster",
+        section3 = "Auto collect rats",
+        section4 = "Auto escape this room",
+        section5 = "Auto escape from Rin",
+        button1 = "Read book",
+        button2 = "ESP Rin",
+        button3 = "Auto collect",
+        button4 = "Auto escape",
+        button5 = "Auto escape Rin",
+        Notification = "Loading...",
+        NotificationContent = "Processing files"
     }
 
-    Tab_Nagisa = {
-        name = "Phase nagisa",
+    local Tab_Nagisa = {
+        name = "Nagisa Phase",
         section1 = "Escape from Nagisa automatically",
-        section2 = "Auto escape nagisa",
-        button1 = "Auto win nagisa",
-        button2 = "Auto escape nagisa"
+        section2 = "Auto escape Nagisa",
+        button1 = "Auto win Nagisa",
+        button2 = "Auto escape Nagisa"
     }
 
-    Tab_PhasesFrame = {
-        name = "Phase kenio",
+    local Tab_PhasesFrame = {
+        name = "Kenio Phase",
         section1 = "Talk to Kenio",
         section2 = "Highlight monster",
         section3 = "Collect key automatically",
@@ -42,53 +42,61 @@ function TranslationModule:GetTabs()
         section6 = "Candle House Teleport",
         section8 = "Collect Orb automatically",
         
-        button1 = "Talk kenio",
-        button2 = "Esp monster",
+        button1 = "Talk to Kenio",
+        button2 = "ESP monster",
         button3 = "Auto collect key",
         button4 = "Auto collect frames",
         button5 = "Show frames",
-        button6 = "Auto Light Candles",
-        button7 = "Teleport to home",
-        button8 = "Teleport to the house of candles",
+        button6 = "Auto light candles",
+        button7 = "Teleport to house",
+        button8 = "Teleport to candle house",
         button9 = "Auto collect orb",
         
-        description1 = "You need to do painting before using this",
+        description1 = "You need to finish the painting before using this",
         description2 = "Use auto collect frames before this function",
-        description3 = "You need to be in the room and have activated Auto collect frames",
+        description3 = "You need to be in the room and have activated auto collect frames",
         
         AddDropdownName = "Houses",
         HouseText = "House "
     }
 
-    Tab_VelaFase = {
-        name = "Fase das velas",
-        section1 = "Teleportar para zona segura",
-        section2 = "Mostrar monstros",
-        section3 = "Teleportar para as velas",
-        section4 = "Escapar de Tenomo",
-        button1 = "Zona segura",
-        button2 = "Esp monstros",
-        button3 = "Teleportar vela",
-        button4 = "Auto escapar",
+    local Tab_VelaFase = {
+        name = "Candles Phase",
+        section1 = "Teleport to safe zone",
+        section2 = "Show monsters",
+        section3 = "Teleport to candles",
+        section4 = "Escape from Tenomo",
+        button1 = "Safe zone",
+        button2 = "ESP monsters",
+        button3 = "Teleport to candle",
+        button4 = "Auto escape",
         AddDropdownName = "Candles",
         NavioVelaText = "Candle "
     }
 
-    Tab_Other = {
-        name = "Outros",
-        section1 = "Mostrar onde o jogador tá",
-        button1 = "Esp player",
-        section2 = "Iluminar ao redor",
+    local Tab_Other = {
+        name = "Others",
+        section1 = "Show where the player is",
+        button1 = "ESP player",
+        section2 = "Light up surroundings",
         button2 = "FullBright"
     }
 
-    Tab_Creditos = {
-        name = "Créditos",
-        paragrafo = "Entre no meu canal do YouTube e no meu Discord para novas atualizações."
+    local Tab_Creditos = {
+        name = "Credits",
+        paragrafo = "Join my YouTube channel and Discord for new updates."
     }
 
-    return Tab_Oceano, Tab_ComedorCarne, Tab_Meat, Tab_Esqueletos, Tab_Alavancas, Tab_Engima, Tab_Comida, Tab_Teiryu, Tab_Torre, Tab_Chihiro, Tab_Boss, Tab_Others, Tab_Creditos
-      
+    return {
+        Tab_Oficine,
+        Tab_Sisters,
+        Tab_Nagisa,
+        Tab_PhasesFrame,
+        Tab_VelaFase,
+        Tab_Other,
+        Tab_Creditos
+    }
+
 end
 
 return TranslationModule
