3ee97652615c17501f71e7f68ec66f5c1d2ee410
raelhubfunctions <raeldevminecraft@gmail.com>
2025-07-06 10:51:45 -0300

    Create en-us.lua

---
+++ Games functions/Brookhaven/brookhaven troll/translation system/languages/en-us.lua
@@ -0,0 +1,121 @@
+local translation = {
+  
+  TLocalPlayer = {
+    
+    ["Name"] = "LocalPlayer",
+    ["Make your player fly"] = "Make your player fly",
+    ["Fly speed"] = "Fly speed",
+    ["Enable speed"] = "Enable speed (You can press F to use fly) ",
+    
+    
+    ["Player cross things"] = "Player cross things",
+    ["Enable Noclip"] = "Enable Noclip",
+    
+    
+    ["Anti seat"] = "Anti seat",
+    ["Anti seat Vehicle"] = "Anti seat Vehicle",
+    ["Anti seat Tool"] = "Anti seat Tool",
+    ["Anti seat All"] = "Anti seat All",
+    
+  },
+    
+  TCar = {
+    
+    ["Name"] = "Car",
+    ["Update car list"] = "Update car list",
+    ["Teleport to the car"] = "Teleport to the car",
+    ["Pull Car"] = "Pull Car",
+    
+  },
+  
+  TAvatar = {
+    
+    ["Name"] = "Avatar",
+    ["Make the player invisible"] = "Make the player invisible (avatar)",
+    ["Become invisible"] = "Become invisible",
+    
+    
+    ["Copy avatar brookhaven"] = "Copy avatar brookhaven",
+    ["Copy avatar type"] = "Copy avatar type",
+    ["Copy avatar"] = "Copy avatar",
+    ["Copy avatar of nearest player"] = "Copy avatar of nearest player",
+    ["Copy random player"] = "Copy random player",
+    
+    
+    ["Copy only the player profile accessories"] = "Copy only the player profile accessories",
+    ["Player Nickname"] = "Player Nickname",
+    ["Enter the player's nickname"] = "Enter the player's nickname",
+    
+  },
+  
+  TKill = {
+
+    ["Name"] = "Kill",
+    ["Kill the players"] = "Kill the player",
+    ["Kill player"] = "Kill player"
+
+  },
+
+  TBring = {
+    ["Name"] = "Bring",
+    ["Bring the players"] = "Bring the player",
+    ["Bring player"] = "Bring player",
+    ["Couch  [method 1]"] = "Couch  [method 1]",
+    ["Couch  [method 2]"] = "Couch  [method 2]"
+
+  },
+
+  TFling = {
+
+    ["Name"] = "Fling",
+    ["Fling the players"] = "Kill the player",
+    ["Fling player"] = "Kill player",
+    ["Fling players with your ball aura"] = "Fling players with your ball aura",
+    ["Fling players with your ball aura (Do not use with fly)"] = "Fling players with your ball aura (Do not use with fly)",
+    ["Fling Car"] = "Fling Car",
+    ["Fling Car [BETA]"] = "Fling Car [BETA]"
+
+  },
+
+  TAudio = {
+
+    ["Name"] = "Audio All",
+    ["Audio list"] = "Audio list",
+    ["Play audio"] = "Play audio",
+    ["Play audio loop"] = "Play audio loop",
+
+    ["Add an id"] = "Add an id",
+    ["Audio all through roblox ids"] = "Audio all through roblox ids",
+    ["Enter a valid id"] = "Enter a valid id"
+
+
+  },
+
+  TLagServer = {
+    ["Name"] = "Lag server",
+    ["Lag Server [Bomb]"] = "Lag Server [Bomb]",
+    ["Lag Server [Laptop]"] = "Lag Server [Laptop]",
+    ["Lag Server [Basketball]"] = "Lag Server [Basketball]",
+    ["Return to normal (rejoin)"] = "Return to normal (rejoin)"
+  },
+  
+  RepeatedWords = {
+
+    ["Player List"] = "Player List",
+    ["Update list"] = "Update list",
+    ["Car list"] = "Car list",
+    ["Select mode"] = "Select mode",
+    ["Spectate player"] = "Spectate player",
+    ["Teleport to player"] = "Teleport to player",
+    ["Teleport to player with car"] = "Teleport to player with car",
+    ["Bus"] = "Bus",
+    ["Couch"] = "Couch",
+    ["Boat"] = "Boat",
+    ["Ball"] = "Ball",
+    ["List of cars in the game"] = "List of cars in the game"
+
+  }
+  
+}
+
+return translation
