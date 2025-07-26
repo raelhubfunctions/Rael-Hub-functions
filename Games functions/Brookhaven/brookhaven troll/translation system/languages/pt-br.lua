d786503b1ab2fc253147ce81b5e3356747ea67d4
raelhubfunctions <raeldevminecraft@gmail.com>
2025-07-26 03:05:07 -0300

    Update pt-br.lua

--- Games functions/Brookhaven/brookhaven troll/translation system/languages/pt-br.lua
+++ Games functions/Brookhaven/brookhaven troll/translation system/languages/pt-br.lua
@@ -1,101 +1,133 @@
 local translation = {
   
   TLocalPlayer = {
-    ["Name"] = "JogadorLocal",
-    ["Make your player fly"] = "Fazer seu jogador voar",
-    ["Fly speed"] = "Velocidade de voo",
-    ["Enable speed"] = "Ativar velocidade (Você pode pressionar F para voar)",
     
-    ["Player cross things"] = "Jogador atravessa objetos",
+    ["Name"] = "Jogador Local",
+    ["Make your player fly"] = "Fazer seu jogador voar",
+    ["Fly speed"] = "Velocidade do voo",
+    ["Enable speed"] = "Ativar voo (Você pode pressionar F para voar)",
+    
+    
+    ["Player cross things"] = "Jogador atravessar objetos",
     ["Enable Noclip"] = "Ativar Noclip",
     
-    ["Anti seat"] = "Anti assento",
-    ["Anti seat Vehicle"] = "Anti assento Veículo",
-    ["Anti seat Tool"] = "Anti assento Ferramenta",
-    ["Anti seat All"] = "Anti assento Tudo",
+
+    ["Stay when locking player"] = "Ficar ao travar jogador",
+    ["Anti lock player"] = "Anti travamento de jogador",
+    
+
+    ["Anti seat"] = "Anti sentar",
+    ["Anti seat Vehicle"] = "Anti sentar em veículo",
+    ["Anti seat Tool"] = "Anti sentar com ferramenta",
+    ["Anti seat All"] = "Anti sentar em tudo",
+    
   },
     
   TCar = {
+    
     ["Name"] = "Carro",
     ["Update car list"] = "Atualizar lista de carros",
     ["Teleport to the car"] = "Teleportar para o carro",
     ["Pull Car"] = "Puxar carro",
+    
   },
   
   TAvatar = {
+    
     ["Name"] = "Avatar",
     ["Make the player invisible"] = "Deixar o jogador invisível (avatar)",
     ["Become invisible"] = "Ficar invisível",
+    
     
     ["Copy avatar brookhaven"] = "Copiar avatar do Brookhaven",
     ["Copy avatar type"] = "Copiar tipo de avatar",
     ["Copy avatar"] = "Copiar avatar",
     ["Copy avatar of nearest player"] = "Copiar avatar do jogador mais próximo",
-    ["Copy random player"] = "Copiar jogador aleatório",
+    ["Copy random player"] = "Copiar avatar de jogador aleatório",
     
-    ["Copy only the player profile accessories"] = "Copiar apenas os acessórios do perfil do jogador",
-    ["Player Nickname"] = "Apelido do Jogador",
+    
+    ["Copy only the player profile accessories"] = "Copiar apenas acessórios do perfil do jogador",
+    ["Player Nickname"] = "Apelido do jogador",
     ["Enter the player's nickname"] = "Digite o apelido do jogador",
+    
+  },
+
+  TLockPlayer = {
+    ["Name"] = "Travar jogadores",
+    ["Lock all players"] = "Travar todos os jogadores",
+    ["Lock Players"] = "Travar jogadores",
+    ["Lock Player"] = "Travar jogador"
+    
   },
   
   TKill = {
+
     ["Name"] = "Matar",
-    ["Kill the players"] = "Matar os jogadores",
+    ["Kill the players"] = "Matar jogadores",
     ["Kill player"] = "Matar jogador"
+
   },
 
   TBring = {
     ["Name"] = "Trazer",
-    ["Bring the players"] = "Trazer os jogadores",
+    ["Bring the players"] = "Trazer jogadores",
     ["Bring player"] = "Trazer jogador",
     ["Couch [method 1]"] = "Sofá [método 1]",
     ["Couch [method 2]"] = "Sofá [método 2]"
+
   },
 
   TFling = {
+
     ["Name"] = "Arremessar",
-    ["Fling the players"] = "Arremessar os jogadores",
+    ["Fling the players"] = "Arremessar jogadores",
     ["Fling player"] = "Arremessar jogador",
     ["Fling players with your ball aura"] = "Arremessar jogadores com sua aura de bola",
     ["Fling players with your ball aura (Do not use with fly)"] = "Arremessar jogadores com sua aura de bola (Não use com voo)",
     ["Ball radius"] = "Raio da bola",
-    ["Fling Car"] = "Arremessar Carro",
-    ["Fling Car [BETA]"] = "Arremessar Carro [BETA]"
+    ["Fling Car"] = "Arremessar carro",
+    ["Fling Car [BETA]"] = "Arremessar carro [BETA]"
+
   },
 
   TAudio = {
-    ["Name"] = "Áudio Geral",
+
+    ["Name"] = "Áudio geral",
     ["Audio list"] = "Lista de áudios",
     ["Play audio"] = "Tocar áudio",
-    ["Play audio loop"] = "Tocar áudio em loop",
+    ["Play audio loop"] = "Repetir áudio",
+
     ["Add an id"] = "Adicionar um ID",
-    ["Audio all through roblox ids"] = "Áudio geral por IDs do Roblox",
+    ["Audio all through roblox ids"] = "Tocar áudio para todos via ID do Roblox",
     ["Enter a valid id"] = "Digite um ID válido"
+
   },
 
   TLagServer = {
-    ["Name"] = "Lag no servidor",
-    ["Lag Server [Bomb]"] = "Lag no Servidor [Bomba]",
-    ["Lag Server [Laptop]"] = "Lag no Servidor [Notebook]",
-    ["Lag Server [Basketball]"] = "Lag no Servidor [Basquete]",
+    ["Name"] = "Travamento de servidor",
+    ["Lag Server [Bomb]"] = "Travar servidor [Bomba]",
+    ["Lag Server [Laptop]"] = "Travar servidor [Laptop]",
+    ["Lag Server [Basketball]"] = "Travar servidor [Basquete]",
     ["Return to normal (rejoin)"] = "Voltar ao normal (reentrar)"
   },
   
   RepeatedWords = {
-    ["Player List"] = "Lista de Jogadores",
+
+    ["Player List"] = "Lista de jogadores",
     ["Update list"] = "Atualizar lista",
-    ["Car list"] = "Lista de Carros",
+    ["Car list"] = "Lista de carros",
     ["Select mode"] = "Selecionar modo",
     ["Spectate player"] = "Espectar jogador",
-    ["Teleport to player"] = "Teleportar para o jogador",
-    ["Teleport to player with car"] = "Teleportar para o jogador com carro",
+    ["Teleport to player"] = "Teleportar para jogador",
+    ["Teleport to player with car"] = "Teleportar para jogador com carro",
     ["Bus"] = "Ônibus",
     ["Couch"] = "Sofá",
     ["Boat"] = "Barco",
     ["Ball"] = "Bola",
     ["List of cars in the game"] = "Lista de carros no jogo"
-  }
 
+  }
+  
 }
 
 return translation
