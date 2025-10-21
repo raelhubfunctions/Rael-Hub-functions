194b4d3c698e33d87faf0139fb675b00d00d46d6
Rael_Dev <195318098+raelhubfunctions@users.noreply.github.com>
2025-10-20 21:01:40 -0300

    fixed esp: part in grinDemon

--- Rael functions/script.lua
+++ Rael functions/script.lua
@@ -118,11 +118,21 @@ function raelhubfunctions.CreateEspDistance(object: Instance, color: Color3, tex
   end
 
   local objectPart: BasePart | MeshPart;
+  local objects: { Instance } = object:GetDescendants()
 
-  for _, part: BasePart | MeshPart in ipairs(object:GetDescendants()) do
-    if part.Name == "HumanoidRootPart" or part:IsA("BasePart") or part:IsA("MeshPart") then 
+  for _, part: BasePart | MeshPart in ipairs(objects) do
+    if part.Name == "HumanoidRootPart" and part:IsA("BasePart") or part:IsA("MeshPart")then
       objectPart = part
       break
+    end
+  end
+  
+  if not objectPart then
+    for _, part: BasePart | MeshPart in ipairs(objects) do
+      if part:IsA("BasePart") or part:IsA("MeshPart") then
+        objectPart = part
+        break
+      end
     end
   end
 
